0add30ecac82e53cf180430fef223943d6e4f15e
Vishal Cherian <vish.cherian@gmail.com>
2021-03-07 23:00:52 -0500

    initial commit ☕

---
+++ src/screens/HomeScreen.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const LoginScreen = ( props ) => {
+    return (
+        <h1>Home Screen</h1>
+    )
+}
+
+export default LoginScreen
---
+++ src/screens/ProfileScreen.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const LoginScreen = ( props ) => {
+    return (
+        <h1>Profile Screen</h1>
+    )
+}
+
+export default LoginScreen
